8767791fe26e6b3f8dbefdf68727b8372a0f46bc
Santiago Bonessi <santiagobonessi@hotmail.com>
2021-07-25 20:50:47 +1000

    Add chart and chart bar widgets

--- lib/main.dart
+++ lib/main.dart
@@ -49,6 +49,16 @@ class _MyHomePageState extends State<MyHomePage> {
     // Transaction(id: 'T2', title: 'New phone', amount: 900.00, date: DateTime.now()),
   ];
 
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((tx) {
+      return tx.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+        ),
+      );
+    }).toList();
+  }
+
   void _addNewTransaction(String txTitle, double txAmount) {
     final newTx = Transaction(
       id: DateTime.now().toString(),
@@ -91,7 +101,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Chart(),
+            Chart(_recentTransactions),
             TransactionList(_userTransactions),
           ],
         ),
