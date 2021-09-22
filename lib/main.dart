ce0c9ef1f5ed46252b4203afdc87785ea74a005e
Santiago Bonessi <santiagobonessi@hotmail.com>
2021-09-22 10:00:44 +1000

    Add widget binding observer

--- lib/main.dart
+++ lib/main.dart
@@ -50,8 +50,26 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   bool _showChart = false;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    print(state);
+    super.didChangeAppLifecycleState(state);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
 
   final List<Transaction> _userTransactions = [];
 
